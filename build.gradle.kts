f66ed79d6299804a7218a0e8db63f1295ee711ff
Rocky1884 <manjumahesh2205@gmail.com>
2026-07-14 22:12:44 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.4.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.4.0" apply false
     id("com.android.library") version "8.4.0" apply false
